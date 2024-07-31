62c1f0697c71b77f81be1c7b7e6a2419f10eb719
suhyeoonn <suhyeon1740@gmail.com>
2024-07-31 16:59:44 +0900

    ♻️ 거래내역 리스트 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import TransactionList from "@/components/ui/transactionList";
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
@@ -52,74 +53,8 @@ export default function Home() {
             </CardContent>
           </Card>
         </div>
-        <Card className="w-full">
-          <CardHeader className="border-b p-3">
-            <CardTitle className="font-normal">
-              <div className="flex justify-between">
-                <div className="flex gap-1 items-center">
-                  <span>01</span>
-                  <span>금요일</span>
-                  <span className="text-xs text-muted-foreground">2023.12</span>
-                </div>
-                <div className="flex gap-5">
-                  <span className="text-blue-500 font-medium">0,000,000원</span>
-                  <span className={`${expenseTextColor} font-medium`}>
-                    0,000,000원
-                  </span>
-                </div>
-              </div>
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="p-3">
-            <ul>
-              <li className="grid grid-cols-3 p-2">
-                <span className="text-sm text-muted-foreground">category</span>
-                <span className="font-medium leading-none">content</span>
-                <span className={`${incomeTextColor} text-right`}>0,000원</span>
-              </li>
-              <li className="grid grid-cols-3 p-2">
-                <span className="text-sm text-muted-foreground">category</span>
-                <span className="font-medium leading-none">content</span>
-                <span className={`${incomeTextColor} text-right`}>0,000원</span>
-              </li>
-              <li className="grid grid-cols-3 p-2">
-                <span className="text-sm text-muted-foreground">category</span>
-                <span className="font-medium leading-none">content</span>
-                <span className={`${incomeTextColor} text-right`}>0,000원</span>
-              </li>
-            </ul>
-          </CardContent>
-        </Card>
-        <Card className="w-full">
-          <CardHeader className="border-b p-3">
-            <CardTitle className="font-normal">
-              <div className="flex justify-between">
-                <div className="flex gap-1 items-center">
-                  <span>01</span>
-                  <span>금요일</span>
-                  <span className="text-xs text-muted-foreground">2023.12</span>
-                </div>
-                <div className="flex gap-5">
-                  <span className={`${incomeTextColor} font-medium`}>
-                    0,000,000원
-                  </span>
-                  <span className={`${expenseTextColor} font-medium`}>
-                    0,000,000원
-                  </span>
-                </div>
-              </div>
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="p-3">
-            <ul>
-              <li className="grid grid-cols-3 p-2">
-                <span className="text-sm text-muted-foreground">category</span>
-                <span className="font-medium leading-none">content</span>
-                <span className={`${incomeTextColor} text-right`}>0,000원</span>
-              </li>
-            </ul>
-          </CardContent>
-        </Card>
+        <TransactionList />
+        <TransactionList />
       </div>
     </main>
   );
