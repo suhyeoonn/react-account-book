b5f886a0cb4103f449f8fffc83c6fcc936fd4e09
suhyeoonn <suhyeon1740@gmail.com>
2024-08-01 16:01:24 +0900

    💄 거래내역 삭제 버튼 추가

--- components/transaction/transactionListItem.tsx
+++ components/transaction/transactionListItem.tsx
@@ -1,9 +1,13 @@
 import { getColorByType } from "@/hooks/transactionHooks";
 import { Transaction } from "@/lib/types";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 export default function TransactionListItem({ data }: { data: Transaction }) {
   return (
-    <li className="grid grid-cols-3 p-2">
+    <li className="grid grid-cols-[auto_1fr_1fr_1fr] py-2 gap-2 items-center">
+      <span className="text-destructive text-right hover:bg-destructive hover:text-white rounded p-1">
+        <Cross2Icon />
+      </span>
       <span className="text-sm text-muted-foreground">{data.category}</span>
       <span className="font-medium leading-none">{data.content}</span>
       <span className={`${getColorByType(data.type)} text-right`}>
