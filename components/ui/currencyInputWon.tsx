a139f16c024998615c35cc63d9670160fb5a9f35
suhyeoonn <suhyeon1740@gmail.com>
2024-08-13 02:26:17 +0900

    🐛 CurrencyWon에 forwardRef 추가하여 React.forwardRef() 에러 수정

--- components/ui/currencyInputWon.tsx
+++ components/ui/currencyInputWon.tsx
@@ -1,21 +1,29 @@
 import { NumericFormat } from "react-number-format";
 import { Input } from "./input";
+import { forwardRef } from "react";
 
-export default function CurrencyWon({
-  displayType = "input",
-  ...props
-}: {
-  [x: string]: any;
-  displayType?: "input" | "text";
-}) {
-  return (
-    <NumericFormat
-      className="bg-inherit"
-      customInput={Input}
-      thousandSeparator
-      {...props}
-      suffix={"원"}
-      displayType={displayType}
-    />
-  );
-}
+const CurrencyWon = forwardRef(
+  (
+    {
+      displayType = "input",
+      ...props
+    }: {
+      [x: string]: any;
+      displayType?: "input" | "text";
+    },
+    ref
+  ) => {
+    return (
+      <NumericFormat
+        className="bg-inherit"
+        customInput={Input}
+        thousandSeparator
+        {...props}
+        suffix={"원"}
+        displayType={displayType}
+      />
+    );
+  }
+);
+
+export default CurrencyWon;
