b8ddbd1b44167e0d6b07363cda7210cec8036ed1
suhyeoonn <suhyeon1740@gmail.com>
2024-08-19 03:35:12 +0900

    ✨ 카테고리 설정 DB 연동

--- components/category/categoryList.tsx
+++ components/category/categoryList.tsx
@@ -1,41 +1,23 @@
-"use client";
-
-import { category as dummy } from "@/lib/placeholder-data";
-import { Category, TransactionType } from "@/lib/types";
+import { TransactionType } from "@/lib/types";
 import CategoryForm from "./categoryForm";
-import { useEffect, useState } from "react";
 import CategoryListItem from "./categoryListItem";
+import { getCategoryList } from "@/lib/actions";
 
-export default function CategoryList({ type }: { type: TransactionType }) {
-  const [categoryList, setCategoryList] = useState<Category[]>([]);
-
-  useEffect(() => {
-    setCategoryList(dummy);
-  }, [type]);
-
-  const addCategory = (categoryName: string) => {
-    setCategoryList([
-      ...categoryList,
-      {
-        id: categoryList.length + 1,
-        name: categoryName,
-        type,
-      },
-    ]);
-  };
-
-  const deleteCategory = (id: number) => {
-    setCategoryList(categoryList.filter((c) => c.id !== id));
-  };
+export default async function CategoryList({
+  type,
+}: {
+  type: TransactionType;
+}) {
+  const categoryList = await getCategoryList();
 
   return (
     <ul>
       {categoryList
         .filter((c) => c.type === type)
         .map((c) => (
-          <CategoryListItem key={c.id} category={c} onDelete={deleteCategory} />
+          <CategoryListItem key={c.id} category={c} />
         ))}
-      <CategoryForm onSubmit={addCategory} />
+      <CategoryForm />
     </ul>
   );
 }
